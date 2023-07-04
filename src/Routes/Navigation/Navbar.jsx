3154061ec78ed1fbafcbd06eeaf98021aa63fd7f
Gele <gellehussen@gmail.com>
2023-07-04 18:54:52 +0200

    intiliazed project and created base start for navbar

---
+++ src/Routes/Navigation/Navbar.jsx
@@ -0,0 +1,40 @@
+
+
+const Navbar = ()=>{
+
+    return(
+
+        <div    className="navbar">
+
+        <div className="navTop"> 
+
+        <div className="profile"> </div>
+
+        <div    className="Logo"></div>
+
+        <div   className="Cart"></div>
+        </div>
+
+     
+
+        <div    className="navBottom">
+
+        <div    className="navLinks">
+            <ul>
+              
+                <li>New Arrivals</li>
+                <li> Brands</li>
+                <li>Garments</li>
+                <li>Shoes</li>
+                <li>Shop By</li>
+                <li>Magazine</li>
+            </ul>
+        </div>
+
+        </div>
+        </div>
+    )
+}
+
+
+export default Navbar;
