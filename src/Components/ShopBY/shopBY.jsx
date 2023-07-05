9397b0a90c1ddc59067675d1b8cfed3bed3e77d3
Gele <gellehussen@gmail.com>
2023-07-05 15:18:34 +0200

    Styled ShopBY for the bigger screen

--- src/Components/ShopBY/shopBY.jsx
+++ src/Components/ShopBY/shopBY.jsx
@@ -1,32 +1,39 @@
 import './shopBYStyle.css'
+import '../../Routes/Navigation/nav.css';
 
-const ShopBY = ()=>{
+const ShopBY = ({isShopBY,setShopBY})=>{
+  console.log(isShopBY)
 
+  //When a user clicks on one of the links it will set the drop down to false
+  const changeHandler = ()=>{
+    setShopBY(false)
+  }
+  console.log(isShopBY)
     return(
-        <div className="shopBy">
+        <div className= {isShopBY ? "shopBy dropShow" : "shopBy"}>
             <div  className="sec1">
             <h3> For you</h3>
             <div className="sec1Main">
-                <ul>
-                    <li>Example</li>
-                    <li>Example</li>
-                    <li>Example</li>
-                    <li>Example</li>
-                    <li>Example</li>
+                <ul >
+                    <li onClick={changeHandler}>Example</li>
+                    <li onClick={changeHandler}>Example</li>
+                    <li onClick={changeHandler}>Example</li>
+                    <li onClick={changeHandler}>Example</li>
+                    <li onClick={changeHandler}>Example</li>
                    
                 </ul>
             </div>
             </div>
 
             <div    className="sec2">
-            <h3> Discover more</h3>
+            <h3 > Discover more</h3>
             <div className="sec2Main">
             <ul>
-                    <li>Example</li>
-                    <li>Example</li>
-                    <li>Example</li>
-                    <li>Example</li>
-                    <li>Example</li>
+                    <li onClick={changeHandler} >Example</li>
+                    <li onClick={changeHandler}>Example</li>
+                    <li onClick={changeHandler}>Example</li>
+                    <li onClick={changeHandler}>Example</li>
+                    <li onClick={changeHandler}>Example</li>
                    
                 </ul>
                 </div>
--- src/Routes/Navigation/Navbar.jsx
+++ src/Routes/Navigation/Navbar.jsx
@@ -2,7 +2,18 @@ import logo from '../../Assets/logo.png';
 import cartIcon from '../../Assets/shopping-cart.svg';
 import userIcon from '../../Assets/user.svg';
 import './nav.css';
+import '../../Components/ShopBY/shopBYStyle.css';
+import ShopBY from '../../Components/ShopBY/shopBY';
+import { useState } from 'react';
 const Navbar = ()=>{
+        //We use usestate to determine if we show the drop down or not
+        // Use state can be used to store many thing such as arrays, etcs not just boolean 
+        //In this case see it as a variable with boolean expersion 
+        //which changes based on the action performed
+        const [isShopBY, setShopBY]= useState(false);
+
+
+    
 
     return(
 
@@ -39,12 +50,20 @@ const Navbar = ()=>{
                 <li><a href='#'>  Brands</a></li>
                 <li><a href='#'> Garments</a></li>
                 <li><a href='#'> Shoes</a></li>
-                <li className='shopBY'><a href='#'> Shop By</a></li>
+                {/* When a user hovers over shop by its set to true and shows the drop down
+                    When it leaves it set to false
+                */}
+                <li onMouseEnter={()=>setShopBY(true)}  onMouseLeave={()=> setShopBY(false)}>
+                    <a href='#'> Shop By</a>
+                   { <ShopBY isShopBY={isShopBY}  setShopBY={setShopBY}/>}
+                    </li>
                 <li><a href='#'> Magazine</a> </li>
             </ul>
         </div>
 
         </div>
+
+
         </div>
     )
 }
