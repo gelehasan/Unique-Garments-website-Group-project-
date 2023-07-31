98bebb4ebc3b68c5fe0021020bbe3ae3e66becf0
PhoenixMyomyat <86453618+PhoenixMyomyat@users.noreply.github.com>
2023-07-31 20:57:46 +0630

    Update SignIn.jsx for sign in

--- src/Routes/SignIn/SignIn.jsx
+++ src/Routes/SignIn/SignIn.jsx
@@ -3,8 +3,11 @@ import './SignInStyle.css';
 const SignIn = ()=>{
 
     return(
-        <div>
-
+        <div class="container">
+            <h1>Sign In</h1>
+            <input type="text" placeholder="Username" id="username" required>
+            <input type="password" placeholder="Password" id="password" required>
+            <button type="button">Log In</button>
         </div>
     )
 }
