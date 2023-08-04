da8f8f689e139613b0dfc8f1e7cc7ddadf10781e
Gele <gellehussen@gmail.com>
2023-08-04 20:23:51 +0200

    Styled top section filter options

--- src/Routes/Garments/GarmentsPage.jsx
+++ src/Routes/Garments/GarmentsPage.jsx
@@ -2,11 +2,11 @@ import './GarmentsStyling.css';
 import { useEffect, useState } from 'react';
 const brandNames = ["Adidas", "Balenciaga", "Converse", "J.Crew", "Nike", "Off-white", "Rick Owens"];
 const colors = ["white", "black", "purple","blue"];
-
+const accessoriesNames=["Hats","Sunglass", "Belts","Socks"];
 const Garments = ()=>{
     const [selectedBrand, setSelectedBrand] = useState(["All"]);
     const [selectedColor, setSelectedColor]= useState(["All"]);
-  
+    const [selectedCatagory, setselectedCatagory] = useState("All")
  
     const handleBrandChange = (brand) => {
         if(brand==selectedBrand){
@@ -23,18 +23,68 @@ const Garments = ()=>{
         }   
       }
 
-      
     
-   
 
     return(
         <div>
+<div className='topContainer'> 
+<div className='topSub'>
+   
+   <label className='BrandTitle topTitles'> Accessories</label> 
+    <div className='BrandSelection catagoriesSelection'>
+      <ul>
+        <li>View All</li>
+      {accessoriesNames.map((names)=>{
+      return(<li key={names}> {names}</li>) 
+      }
+      )
+
+      }
+
+      </ul>
+    </div>
+   </div>
+
+   <div className='topSub'>
+   
+   <label className='BrandTitle topTitles'> Bags</label> 
+    <div className='BrandSelection catagoriesSelection'>
+      <ul>
+        <li>View All</li>
+      {accessoriesNames.map((names)=>{
+      return(<li key={names}> {names}</li>) 
+      }
+      )
+
+      }
+
+      </ul>
+    </div>
+   </div>
+
+   <div className='topSub'>
+   
+   <label className='BrandTitle topTitles'> Clothing</label> 
+    <div className='BrandSelection catagoriesSelection'>
+      <ul>
+        <li>View All</li>
+      {accessoriesNames.map((names)=>{
+      return(<li key={names}> {names}</li>) 
+      }
+      )
+
+      }
+
+      </ul>
+    </div>
+   </div>
+   </div>
+
 
       <div className='bottomContainer'>
    
         <label className='BrandTitle'> Brands</label> 
            
-      
        
        <div>  {brandNames.map((brand) => (
           <div key={brand} className='BrandSelection'>
