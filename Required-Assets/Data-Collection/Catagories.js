dd28b6c1eb9d35f713795a86b238b504456748e1
Anjelo <141374314+anjelomati@users.noreply.github.com>
2023-08-09 20:25:39 -0400

    Added Item - Hats

---
+++ Required-Assets/Data-Collection/Catagories.js
@@ -0,0 +1,7 @@
+{ "id": 1, 
+"title": "Bookish Dril Baseball Cap", 
+"price": 445, 
+"description": "Your everyday baseball cap. Simple but elegant.", "category": "Hats", 
+"image": "https://asset.cloudinary.com/dmhnsp8sj/c63a95974a33bbc4e2a11343abcced76, "occasions": ["Casual"], 
+"brand": "Balenciaga", 
+"color": "Blue" }
