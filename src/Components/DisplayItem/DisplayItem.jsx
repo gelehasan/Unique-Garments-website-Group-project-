f3bf8ec6b886af9afd737ea22ef36912dfaa8fe2
Anjelo <141374314+anjelomati@users.noreply.github.com>
2023-08-17 20:07:37 -0400

    Update DisplayItem.jsx

--- src/Components/DisplayItem/DisplayItem.jsx
+++ src/Components/DisplayItem/DisplayItem.jsx
@@ -4,7 +4,174 @@ const DisplayItem = ()=> {
 
     return(
     <> 
-    {/**Paste your html code here */}
+    {<html>
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Nanum+Myeongjo:wght@800&display=swap" rel="stylesheet">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Libre+Baskerville&display=swap" rel="stylesheet">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Abel&display=swap" rel="stylesheet">
+    <link rel="stylesheet" type="text/css" href="index.css" />
+    <link href="https://fonts.googleapis.com/css2?family=Abel&family=Cormorant+Garamond:wght@300&display=swap" rel="stylesheet">
+
+    <div class="divHeader">
+        <header>
+            <a class="signUp" href="url">Sign Up / Login</a>
+            <a class="cart" href="url">Cart &#128722; </a>
+            <h1 id="header">Unique Garments</h1>
+        </header>
+    </div>
+
+
+    <head>
+        <title>Bookish Drill Baseball Cap</title>
+        <meta name="description" content="Our first page">
+        <meta name="keywords" content="html tutorial template">
+        </head>
+
+    <div class="container">
+    
+    <div class="itemImage">
+        
+        <img id="item" src="https://res.cloudinary.com/dmhnsp8sj/image/upload/c_pad,b_auto:predominant,fl_preserve_transparency/v1691626787/Garments/BlueBalenciagaCap.jpg?_s=public-apps" >
+    </div>
+    
+    <div class="itemClass">
+        <br>
+        <br>
+        <br>
+        <br>
+        <br>
+        <br>
+        <br>
+    <h1 id="title">Off-White Bookish Drill Baseball Cap</h1>
+    <h4>Your everyday baseball cap, simple but elegant.</h4>
+    <br>
+    <p>$445.00</p>
+    <p>Colour: Blue</p>
+    <br>
+
+    <div class="sizeGroup">
+        <button>S</button>
+        <button>M</button>
+        <button>L</button>
+        <button>XL</button>
+    </div>
+<br>
+<br>
+
+<button class="button-60" role="button">
+    ADD TO WISHLIST&nbsp;
+    <input type="checkbox" class="heart-checkbox" id="heart-checkbox">
+    <label for="heart-checkbox" class="heart">&#10084;</label>
+</button>
+
+<button class="button-61" role="button">
+    ADD TO SHOPPING CART
+</button>
+
+    </div>
+</div>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+
+
+<footer class="footer">
+    <div class="footer__addr">
+      <h1 class="footer__logo">Unique Garments</h1>
+          
+      <h2>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Singapore</h2>
+      <br>
+
+        <a class="footer__btn" href="mailto:example@gmail.com">Email Us</a>
+      </address>
+    </div>
+    
+    <ul class="footer__nav">
+      <li class="nav__item">
+        <h2 class="nav__title">Quick Links</h2>
+  
+        <ul class="nav__ul">
+          <li>
+            <a href="#">My Account</a>
+          </li>
+  
+          <li>
+            <a href="#">Wishlist</a>
+          </li>
+              
+          <li>
+            <a href="#">Articles</a>
+          </li>
+        </ul>
+      </li>
+      
+      <li class="nav__item nav__item--extra">
+        <h2 class="nav__title">Relevant Links</h2>
+        
+        <ul class="nav__ul nav__ul--extra">
+          <li>
+            <a href="#">About the Website</a>
+          </li>
+          
+          <li>
+            <a href="#">Feedback</a>
+          </li>
+          
+          <li>
+            <a href="#">Customer Care</a>
+          </li>
+          
+          <li>
+            <a href="#">Careers</a>
+          </li>
+          
+          <li>
+            <a href="#">Newsletter Signup</a>
+          </li>
+          
+          <li>
+            <a href="#">Country / Region</a>
+          </li>
+        </ul>
+      </li>
+      
+      <li class="nav__item">
+        <h2 class="nav__title">Legal</h2>
+        
+        <ul class="nav__ul">
+          <li>
+            <a href="#">Privacy Policy</a>
+          </li>
+          
+          <li>
+            <a href="#">Terms of Use</a>
+          </li>
+          
+          <li>
+            <a href="#">Sitemap</a>
+          </li>
+        </ul>
+      </li>
+    </ul>
+    
+
+  </footer>
+
+</html>}
     
     </>
     )
