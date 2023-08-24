f81ffb0bc6c7d28149adcc8c0fe4725bf56475cf
Gele <gellehussen@gmail.com>
2023-08-24 22:17:04 +0200

    Displaying chosen article using useprams

--- src/Components/Article-Components/Article/Article.jsx
+++ src/Components/Article-Components/Article/Article.jsx
@@ -1,45 +1,58 @@
 import { useState, useEffect } from "react";
 import { getArticleData } from "../../../Firebase/firebase";
 import { useParams } from "react-router-dom"
+import "./Article.css"
  const Article = ()=> {
     const {articleID} = useParams();
     const [selectedArticle, setselectedArticle] = useState();
 
-
+    useEffect(()=>{
+        const FetchArticle = async ()=>{
+            try{
+                let ArticlesData = await getArticleData();
+                setselectedArticle(ArticlesData[articleID]);
+            }catch(error){
+               
+            }}
+                FetchArticle();
+        },[])
+        console.log(selectedArticle)
+        
     return(
         <div>
-      { /* selectedArticle && <div class="article">
+      {  selectedArticle && <div class="article">
+       
         <header class="article-header">
             
-            <h1 class="article-title">{articleTitle}</h1>
-            <p class="article-author">{author}</p>
-            <p class="article-date">{publishedDate}</p>
-            <img src={articlePicture} />
+            <h1 class="article-title">{selectedArticle.articleTitle}</h1>
+            <p class="article-author">{selectedArticle.author}</p>
+            <p class="article-date">{selectedArticle.publishedDate}</p>
+            <img src={selectedArticle.articlePicture} />
         </header>
 
         <article class="article-content">
-            <p> {introPassage}</p>
+            <p> {selectedArticle.introPassage}</p>
 
-            <p>{firstPassage}</p>
-
-            <blockquote class="quote">
-                <p>{firstQuote}</p>
-            </blockquote>
-
-           <p>  {secondPassage} </p>
+            <p>{selectedArticle.firstPassage}</p>
 
             <blockquote class="quote">
-                <p>{secondQuote}</p>
+                <p>{selectedArticle.firstQuote}</p>
             </blockquote>
 
-            <p>{thirdPassage}</p>            
+           <p>  {selectedArticle.secondPassage} </p>
+
             <blockquote class="quote">
-                <p>{thirdQuote}</p>
+                <p>{selectedArticle.secondQuote}</p>
             </blockquote>
-            <p>{finalPassage}</p>
+
+            <p>{selectedArticle.thirdPassage}</p>            
+            <blockquote class="quote">
+                <p>{selectedArticle.thirdQuote}</p>
+            </blockquote>
+            <p>{selectedArticle.finalPassage}</p>
         </article>
     </div>
-    */ }
+    }
    </div> 
     )
 }
