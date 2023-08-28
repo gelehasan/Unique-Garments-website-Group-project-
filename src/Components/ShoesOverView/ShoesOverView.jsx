c4fc4d3b46aca28144ec5a528b3bad9d3f6b520e
Gele <gellehussen@gmail.com>
2023-08-29 01:19:11 +0200

    Created Shoes overview

---
+++ src/Components/ShoesOverView/ShoesOverView.jsx
@@ -0,0 +1,36 @@
+import { useSelector } from "react-redux";
+import { getCollectionData } from "../../Firebase/firebase";
+import { setCatagories, setCatagoryLoadError } from "../../Store/Reducers/CatagoriesReducer.js/CatagoryAction";
+import { useEffect } from "react";
+import { useDispatch } from "react-redux";
+import Catagories from "../Categories/Categories";
+import { SelectShoes } from "../../Store/Reducers/CatagoriesReducer.js/CatagorySelector";
+
+
+const ShoesOverView = ()=> {
+    const ShoesData = useSelector(SelectShoes);
+    const dispatch = useDispatch()
+
+    useEffect(()=>{
+        const FetchCatagories = async ()=>{
+            try{
+                let Data = await getCollectionData();
+                dispatch(setCatagories(Data))
+            }catch(error){
+                dispatch(setCatagoryLoadError, error)
+            }}
+            FetchCatagories();
+    },[])
+
+    return(
+        <div>
+
+        { ShoesData.length > 0 && 
+                <Catagories DataShop={ShoesData} isGarmentsFilterOn={false} isShoesFilterOn={true} isShopByFilterOn={false} />
+                 }
+                </div>
+    )
+}
+
+
+export default ShoesOverView;
--- src/Routes/Shoes/ShoesPage.jsx
+++ src/Routes/Shoes/ShoesPage.jsx
@@ -1,34 +1,11 @@
-import './ShoesStyling.css';
-import { useSelector } from 'react-redux';
-import Catagories from '../../Components/Categories/Categories';
-import { useDispatch } from 'react-redux';
-import { useEffect } from 'react';
-import { getCollectionData } from '../../Firebase/firebase';
-import { setCatagories } from '../../Store/Reducers/CatagoriesReducer.js/CatagoryAction';
-import { SelectShoes } from '../../Store/Reducers/CatagoriesReducer.js/CatagorySelector';
-import { setCatagoryLoadError } from '../../Store/Reducers/CatagoriesReducer.js/CatagoryAction';
+import ShoesOverView from "../../Components/ShoesOverView/ShoesOverView";
+import { Routes, Route } from "react-router-dom";
 const Shoes = ()=>{
-    const ShoesData = useSelector(SelectShoes);
-    const dispatch = useDispatch()
-
-    useEffect(()=>{
-        const FetchCatagories = async ()=>{
-            try{
-                let Data = await getCollectionData();
-                dispatch(setCatagories(Data))
-            }catch(error){
-                dispatch(setCatagoryLoadError, error)
-            }}
-            FetchCatagories();
-    },[])
+   
 
     return(
-        <div>
-
-{ ShoesData.length > 0 && 
-        <Catagories DataShop={ShoesData} isGarmentsFilterOn={false} isShoesFilterOn={true} isShopByFilterOn={false} />
-         }
-        </div>
+        <Routes></Routes>
+      
     )
 }
 
