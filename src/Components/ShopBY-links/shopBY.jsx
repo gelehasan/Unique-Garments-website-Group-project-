217017a456b2172132a01858139a4d3f82bef0b8
Gele <gellehussen@gmail.com>
2023-09-04 23:05:39 +0200

    Finilized ShopBy drop down

--- src/Components/ShopBY-links/shopBY.jsx
+++ src/Components/ShopBY-links/shopBY.jsx
@@ -1,10 +1,45 @@
 import './shopBYStyle.css'
 import '../../Routes/Navigation/nav.css';
-
+import { SelectAllCatagories } from '../../Store/Reducers/CatagoriesReducer.js/CatagorySelector';
+import { useEffect, useState } from 'react';
+import { useSelector } from 'react-redux';
+import { Link } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
 const ShopBY = ({isShopBY,setShopBY})=>{
-
-
   //When a user clicks on one of the links it will set the drop down to false
+  const currentUser = useSelector((state)=> state.user.currentUser);
+  const AllCataogires = useSelector(SelectAllCatagories);
+  const [shopBYItems, setShopBYItems] = useState();
+  const [discoverMore,setDiscoverMore] = useState();
+  const Navigate = useNavigate();
+
+    useEffect(()=>{
+    if(currentUser )
+    {
+    const filteredItems=  AllCataogires.filter((items)=> {
+      return items.occasions.toLowerCase() == currentUser.type.toLowerCase()
+     })
+    const shuffledItems = [...filteredItems].sort(() => 0.5 - Math.random());
+      const selectedItems = shuffledItems.slice(0, 5);
+     setShopBYItems(selectedItems)
+
+    }else{
+        const shuffledItems = [...AllCataogires].sort(() => 0.5 - Math.random());
+        const selectedItems = shuffledItems.slice(0, 5);
+        setShopBYItems(selectedItems)
+    }
+    },[currentUser])
+
+    useEffect(()=>{
+        const shuffledItems = [...AllCataogires].sort(() => 0.6 - Math.random());
+        const selectedItems = shuffledItems.slice(0, 6);
+        setDiscoverMore(selectedItems)
+    },[])
+
+  const handleNavigation = (url)=>{
+    Navigate(url)
+    window.location.reload()
+  }
   const changeHandler = ()=>{
     setShopBY(false)
   }
@@ -15,11 +50,19 @@ const ShopBY = ({isShopBY,setShopBY})=>{
             <h3> For you</h3>
             <div className="sec1Main">
                 <ul >
-                    <li onClick={changeHandler}>Example</li>
-                    <li onClick={changeHandler}>Example</li>
-                    <li onClick={changeHandler}>Example</li>
-                    <li onClick={changeHandler}>Example</li>
-                    <li onClick={changeHandler}>Example</li>
+                   {shopBYItems &&
+                    shopBYItems.map((items)=>{
+                        return(
+                            
+                            <li key={items.id} onClick={()=> handleNavigation(`/${items.urlLink}/${items.id}`)}>
+                          
+                              {items.title}
+                           
+                          </li>
+
+                        )
+                    })
+                   }
                    
                 </ul>
             </div>
@@ -29,12 +72,19 @@ const ShopBY = ({isShopBY,setShopBY})=>{
             <h3 > Discover more</h3>
             <div className="sec2Main">
             <ul>
-                    <li onClick={changeHandler} >Example</li>
-                    <li onClick={changeHandler}>Example</li>
-                    <li onClick={changeHandler}>Example</li>
-                    <li onClick={changeHandler}>Example</li>
-                    <li onClick={changeHandler}>Example</li>
-                   
+            {discoverMore &&
+                    discoverMore.map((items)=>{
+                        return(
+                            
+                            <li key={items.id} onClick={()=> handleNavigation(`/${items.urlLink}/${items.id}`)}>
+                          
+                              {items.title}
+                           
+                          </li>
+
+                        )
+                    })
+                   }
                 </ul>
                 </div>
             </div>
