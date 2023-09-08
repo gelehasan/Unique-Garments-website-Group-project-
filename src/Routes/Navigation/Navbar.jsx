2071303df5980c1ceec318e6617073441795873b
Lu Min Kha <luminkha.mdy@gmail.com>
2023-09-08 16:59:25 +0800

    implemented active page  highlight

--- src/Routes/Navigation/Navbar.jsx
+++ src/Routes/Navigation/Navbar.jsx
@@ -9,119 +9,103 @@ import '../../Components/ShopBY-links/shopBYStyle.css';
 import ShopBY from '../../Components/ShopBY-links/shopBY';
 import { useState } from 'react';
 import CartBag from '../../Components/CartBag/CartBag';
-import { useContext } from 'react';
-import { CartShopConext } from '../../Context/cartShopContext';
-
-import { Link } from 'react-router-dom';
-import { NavLink } from 'react-router-dom';
-import { UserContext } from '../../Context/userContext';
-import ProfileDropDown from '../../Components/Profile/ProfileDropDown';
 import { useSelector } from 'react-redux';
 import { useDispatch } from 'react-redux';
 import { setCartVisibility } from '../../Store/Reducers/CartReducer/cartActions';
 import heartIcon from "../../Assets/heart.svg";
+import { NavLink } from 'react-router-dom'; // Import NavLink from React Router
+import ProfileDropDown from '../../Components/Profile/ProfileDropDown';
 
-const Navbar = ()=>{
-        //We use usestate to determine if we show the drop down or not
-        // Use state can be used to store many thing such as arrays, etcs not just boolean 
-        //In this case see it as a variable with boolean expersion 
-        //which changes based on the action performed
-        const [isShopBY, setShopBY]= useState(false);
-        const [showMenu, setshowMenu]= useState();
-       // const {isCartOpen,setIsCartOpen, isCheckoutOpen} = useContext(CartShopConext);
-       
-        const [profileDropDown, setProfileDropDown] = useState(false); 
+import { useContext } from 'react';
+import { CartShopConext } from '../../Context/cartShopContext';
+import { UserContext } from '../../Context/userContext';
 
-        const {currentUser} = useSelector((state)=> state.user)
-        const isCartOpen = useSelector((state)=> state.cart.isCartOpen)
-
-        const dispatch = useDispatch();
-        
-       const setCart = ()=>{
-       
-        dispatch(setCartVisibility(!isCartOpen))
-     
-        }
-   
-        const toggleMenu = ()=>{
-            setshowMenu(!showMenu)
-        }
-    return(
-      <div    className="navbar">
-        
-        <div className="navTop"> 
-
-            <div className="profile"  >
-            {profileDropDown && <ProfileDropDown/>}  
-            <img  src={ userIcon} onClick={()=> setProfileDropDown(!profileDropDown)}/>
-          
-         
-        {currentUser? <span className='displayCurrentUser'> 
-                Hi {currentUser.displayName} </span> : ""
-         }
-        
-         
-          </div>
-        
-       <div    className="Logo">
-       <Link to={"/"}>  <img  src={logo }/></Link>
-        </div>
-
-        <div   className="Cart" >
-   
-        <Link to={"/Wishlist"}>  <img className='WishlistIcon' src={heartIcon} /> </Link>
-        <img  src={cartIcon } onClick={  setCart}/>
-
-
-
-
-        </div>
-        
-        
-        </div>
-        {/** Bottom navbar for links */}
-        <div    className="navBottom">
-        <div    className={ showMenu? "navLinks  ":  "navLinks  navLinkShow"} >
-            <ul>
-                <li > <a href='#'> New Arrivals </a></li>
-                <li><Link to={"/Garments"}> Garments </Link> </li>
-                <li> <Link to={"/Shoes"}> Shoes </Link></li>
-                {/* When a user hovers over shop by its set to true and shows the drop down
-                    When it leaves it set to false
-                */}
-                <li onMouseEnter={()=>setShopBY(true)}  onMouseLeave={()=> setShopBY(false)}>
-                    <Link to={"/ShopBy"}> Shop By </Link>
-                   { <ShopBY isShopBY={isShopBY}  setShopBY={setShopBY}/>}
-                    </li>
-                <li> <Link to={"/Articles"}> Magazine</Link> </li>
-            </ul>
-        </div>
-
-        {/* For closing and opening menu*/}
-        <div    className='closeOpenBtn'>
-
-            {
-                showMenu ? 
-                <img className='closeMenu' src={closeBtn} onClick={toggleMenu} />     
-         
-                 :
-                 <img className='openMenu' src={openBtn} onClick={toggleMenu} />
-                
-            }
+const Navbar = () => {
+  const [isShopBY, setShopBY] = useState(false);
+  const [showMenu, setshowMenu] = useState();
+  const [profileDropDown, setProfileDropDown] = useState(false);
   
+  const { currentUser } = useSelector((state) => state.user);
+  const isCartOpen = useSelector((state) => state.cart.isCartOpen);
 
-  </div>
+  const dispatch = useDispatch();
 
+  const setCart = () => {
+    dispatch(setCartVisibility(!isCartOpen));
+  };
+
+  const toggleMenu = () => {
+    setshowMenu(!showMenu);
+  };
+
+  return (
+    <div className="navbar">
+      <div className="navTop">
+        <div className="profile">
+          {profileDropDown && <ProfileDropDown />}
+          <img src={userIcon} onClick={() => setProfileDropDown(!profileDropDown)} />
+          {currentUser ? (
+            <span className='displayCurrentUser'>Hi {currentUser.displayName}</span>
+          ) : (
+            ""
+          )}
         </div>
-        {
-            isCartOpen &&<CartBag isCheckOutPage={false}/>
-           
-        }  
-       
-        <Outlet></Outlet>
+        <div className="Logo">
+          <NavLink to={"/"} id="Logo-link"><img src={logo} alt="Logo" /></NavLink>
         </div>
-    )
-}
+        <div className="Cart">
+          <NavLink to={"/Wishlist"}><img className='WishlistIcon' src={heartIcon} alt="Wishlist" /></NavLink>
+          <img src={cartIcon} onClick={setCart} alt="Cart" />
+        </div>
+      </div>
 
+      <div className="navBottom">
+        <div className={showMenu ? "navLinks" : "navLinks navLinkShow"}>
+          <ul>
+            <li>
+              <NavLink exact to="/" activeClassName="active-link">
+                Home
+              </NavLink>
+            </li>
+            <li>
+              <NavLink to="/Garments" activeClassName="active-link">
+                Garments
+              </NavLink>
+            </li>
+            <li>
+              <NavLink to="/Shoes" activeClassName="active-link">
+                Shoes
+              </NavLink>
+            </li>
+            <li
+              onMouseEnter={() => setShopBY(true)}
+              onMouseLeave={() => setShopBY(false)}
+            >
+              <NavLink to="/ShopBy" activeClassName="active-link">
+                Shop By
+              </NavLink>
+              {isShopBY && <ShopBY isShopBY={isShopBY} setShopBY={setShopBY} />}
+            </li>
+            <li>
+              <NavLink to="/Articles" activeClassName="active-link">
+                Magazine
+              </NavLink>
+            </li>
+          </ul>
+        </div>
+
+        <div className='closeOpenBtn'>
+          {showMenu ? (
+            <img className='closeMenu' src={closeBtn} onClick={toggleMenu} alt="Close Menu" />
+          ) : (
+            <img className='openMenu' src={openBtn} onClick={toggleMenu} alt="Open Menu" />
+          )}
+        </div>
+      </div>
+      {isCartOpen && <CartBag isCheckOutPage={false} />}
+      <Outlet />
+    </div>
+  );
+};
 
 export default Navbar;
